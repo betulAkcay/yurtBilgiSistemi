6f8af89d7faad19379fb2db37a53c8364c0389c3
betulAkcay <betul.akcy@gmail.com>
2016-01-22 00:28:50 +0200

    Ana menu
    form da kullanıcı yapmak kendisine sunulan menüye göre işlem seçiyor

---
+++ Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ybs
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 formkapa = new Form2();
+            formkapa.Close();
+            Form3 form = new Form3();
+            form.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Form2 formkapa = new Form2();
+            formkapa.Close();
+            Form4 form = new Form4();
+            form.Show();
+            this.Hide();
+        }
+
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Form2 formkapa = new Form2();
+            formkapa.Close();
+            Form5 form = new Form5();
+            form.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 formkapa = new Form2();
+            formkapa.Close();
+            Form10 form = new Form10();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
